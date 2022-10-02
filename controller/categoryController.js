47821dad3e9bc4f08faabaa2ae181cef600133a1
SOURAV <MAIL.MYMAIL11@GMAIL.COM>
2022-10-02 12:50:22 +0530

    category complete with mongo db

--- controller/categoryController.js
+++ controller/categoryController.js
@@ -1,10 +1,13 @@
 const asyncHandler = require('express-async-handler')
 
+const  Category = require('../models/categoryModel')
+
 //@desc      Get all category
 //@route     GET   api/v1/category/getall
 //@access    Public
 const getAllCategory = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: 'get all category' });
+    const catagory = await Category.find()
+    res.status(200).json(catagory);
 })
 
 
@@ -21,12 +24,16 @@ const getSpecificCategory = asyncHandler(async (req, res) => {
 //@route     GET   api/v1/category/add
 //@access    Private
 const addCategory = asyncHandler(async (req, res) => {
-    if (!req.body.catname) {
+    if (!req.body.category_name) {
         res.status(400)
         throw new Error('Enter category name')
     }
 
-    res.status(200).json({ message: 'create catagory' });
+    const catagory = await Category.create({
+        category_name : req.body.category_name
+    })
+
+    res.status(200).json(catagory);
 })
 
 
@@ -34,7 +41,24 @@ const addCategory = asyncHandler(async (req, res) => {
 //@route     GET   api/v1/category/update/:id
 //@access    Private
 const updateCategory = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: 'catagory update :' + req.params.id });
+
+    if (!req.body.category_name) {
+        res.status(400)
+        throw new Error('Enter updated category name')
+    }
+
+
+    const category = await Category.findById(req.params.id)
+    if(!category){
+        res.status(400)
+        throw new Error('Category Not found')
+    }
+
+    const updatecategory = await Category.findByIdAndUpdate(req.params.id,req.body,{
+        new:true
+    })
+
+    res.status(200).json({ message: 'catagory update :' + updatecategory });
 })
 
 
@@ -42,6 +66,16 @@ const updateCategory = asyncHandler(async (req, res) => {
 //@route     GET   api/v1/category/delete/:id
 //@access    Private
 const deleteCategory = asyncHandler(async (req, res) => {
+
+    
+    const category = await Category.findById(req.params.id)
+    if(!category){
+        res.status(400)
+        throw new Error('Category Not found')
+    }
+
+    await category.remove()
+
     res.status(200).json({ message: 'delete cat :' + req.params.id });
 })
 
---
+++ models/categoryModel.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+
+const categorySchema = mongoose.Schema({
+    category_name: {
+        type: String,
+        requird: [true, 'Enter category name'],
+        
+    }, status: {
+        type: Boolean,
+        default: true
+    }
+}, {
+    timestamps: true
+})
+
+module.exports = mongoose.model('Category', categorySchema)
